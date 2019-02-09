25a4d45a01a65e73e8523d49bc315aa7fd5dc6f9
Karin8 <kira4074@gmail.com>
2019-02-09 10:43:26 +0300

    Выделение параметров и объединение их в класс ContactData

--- addressbook-web-tests/src/test/java/ru/srqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/srqa/pft/addressbook/ContactCreationTests.java
@@ -13,43 +13,43 @@ public class ContactCreationTests {
     wb = new FirefoxDriver();
     wb.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wb.get("http://localhost/addressbook/");
-    login();
+    login("admin", "secret");
   }
 
-  private void login() {
+  private void login(String username, String password) {
     wb.findElement(By.name("user")).click();
     wb.findElement(By.name("user")).clear();
-    wb.findElement(By.name("user")).sendKeys("admin");
+    wb.findElement(By.name("user")).sendKeys(username);
     wb.findElement(By.name("pass")).click();
     wb.findElement(By.name("pass")).clear();
-    wb.findElement(By.name("pass")).sendKeys("secret");
+    wb.findElement(By.name("pass")).sendKeys(password);
     wb.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
   }
 
   @Test
   public void testContactCreation() throws Exception {
     initContactCreation();
-    fillContactForm();
+    fillContactForm(new ContactData("Karina", "Kuznetsova", "Saint-Petersburg", "80001112233", "email@email.com"));
     submitContactCreation();
     returnToHomePage();
   }
 
-  private void fillContactForm() {
+  private void fillContactForm(ContactData contactData) {
     wb.findElement(By.name("firstname")).click();
     wb.findElement(By.name("firstname")).clear();
-    wb.findElement(By.name("firstname")).sendKeys("Karina");
+    wb.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
     wb.findElement(By.name("lastname")).click();
     wb.findElement(By.name("lastname")).clear();
-    wb.findElement(By.name("lastname")).sendKeys("Kuznetsova");
+    wb.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
     wb.findElement(By.name("address")).click();
     wb.findElement(By.name("address")).clear();
-    wb.findElement(By.name("address")).sendKeys("Saint-Petersburg");
+    wb.findElement(By.name("address")).sendKeys(contactData.getAddress());
     wb.findElement(By.name("mobile")).click();
     wb.findElement(By.name("mobile")).clear();
-    wb.findElement(By.name("mobile")).sendKeys("80001112233");
+    wb.findElement(By.name("mobile")).sendKeys(contactData.getMobilePhone());
     wb.findElement(By.name("email")).click();
     wb.findElement(By.name("email")).clear();
-    wb.findElement(By.name("email")).sendKeys("email@email.com");
+    wb.findElement(By.name("email")).sendKeys(contactData.getEmail());
   }
 
   private void returnToHomePage() {
