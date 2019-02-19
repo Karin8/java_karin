32d5d412e2ee36281e3eeba84c969ab92eb9c8d3
Karin8 <kira4074@gmail.com>
2019-02-19 12:05:10 +0300

    Оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/srqa/pft/addressbook/appManager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/srqa/pft/addressbook/appManager/NavigationHelper.java
@@ -1,6 +1,7 @@
 package ru.srqa.pft.addressbook.appManager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -14,10 +15,18 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void goToGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("groups"));
   }
 
   public void goToHomePage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
     click(By.linkText("home"));
   }
 
