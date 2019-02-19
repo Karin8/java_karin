70290a3bc00e8e278ecb21ebfc47ee7829523bec
Karin8 <kira4074@gmail.com>
2019-02-19 08:34:51 +0300

    Реализован запуск тестов в браузерах Chrome и Internet Explorer

--- addressbook-web-tests/src/test/java/ru/srqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/srqa/pft/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.srqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.srqa.pft.addressbook.appManager.ApplicationManager;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
