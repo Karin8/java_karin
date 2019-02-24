f51d25fa53174c6f6f1defa9a971e7a72355533e
Karin8 <kira4074@gmail.com>
2019-02-24 16:44:06 +0300

    Реализация поиска максимального значения с помощью лямбда-выражения для Group/ContactCreationTests

--- addressbook-web-tests/src/test/java/ru/srqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/srqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.srqa.pft.addressbook.model.ContactData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,18 +14,12 @@ public class ContactCreationTests extends TestBase {
   public void testContactCreation() throws Exception {
     app.getNavigationHelper().goToHomePage();
     List<ContactData> before = app.getContactHelper().getContactList();
-    ContactData contact = new ContactData("Karina", "Kuznetsova", "Saint-Petersburg", "80001112233", "email@email.com");
+    ContactData contact = new ContactData("test", "Kuznetsova", "Saint-Petersburg", "80001112233", "email@email.com");
     app.getContactHelper().createContact(contact);
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (ContactData c : after) {
-      if (c.getId() > max) {
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
