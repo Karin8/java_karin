e500a7958687c9e99ca5e610f0bb7d734e382237
Karin8 <kira4074@gmail.com>
2019-03-08 13:45:51 +0300

    Лекция 6.4. Параметризация тестовых методов

--- addressbook-web-tests/src/test/java/ru/srqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/srqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,23 +1,35 @@
 package ru.srqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.srqa.pft.addressbook.model.ContactData;
 import ru.srqa.pft.addressbook.model.Contacts;
+import ru.srqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new ContactData().withFirstname("firstname1").withLastname("lastname1").withEmail1("email1").withAddress("address1").withHomePhone("111")});
+    list.add(new Object[] {new ContactData().withFirstname("firstname2").withLastname("lastname2").withEmail1("email2").withAddress("address2").withHomePhone("222")});
+    list.add(new Object[] {new ContactData().withFirstname("firstname3").withLastname("lastname3").withEmail1("email3").withAddress("address3").withHomePhone("333")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) {
     app.goTo().homePage();
     Contacts before = app.contact().all();
     File photo = new File("src/test/resources/stesting.png");
-    ContactData contact = new ContactData()
-            .withFirstname("Karina").withLastname("Kuznetsova").withPhoto(photo).withAddress("Saint-Petersburg").withHomePhone("111").withMobilePhone("222").withWorkPhone("333").withEmail1("email@email.com");
     app.contact().create(contact);
     assertThat(app.contact().count(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
