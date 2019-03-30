2e8e7dc348ea01e3509987b27092efec4663fea1
Karin8 <kira4074@gmail.com>
2019-03-30 14:15:04 +0300

    Задание №20: Реализовать интеграцию тестов с баг-трекером Bugify

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
@@ -15,10 +15,11 @@ import java.util.Set;
 
 import static org.testng.Assert.assertEquals;
 
-public class RestTests {
+public class RestTests extends TestBase{
 
   @Test
   public void testCreateIssue() throws IOException {
+    skipIfNotFixed(1015);
     Set<Issue> oldIssues = getIssues();
     Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
     int issueId = createIssue(newIssue);
@@ -35,7 +36,7 @@ public class RestTests {
     return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
   }
 
-  private Executor getExecutor() {
+  public Executor getExecutor() {
     return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
   }
 
