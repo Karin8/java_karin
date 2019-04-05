0d9037537a0f0645052f4ae9d0b7bc90bd5fa317
Karin8 <kira4074@gmail.com>
2019-04-05 22:03:55 +0300

    удалён негативный тест

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -11,11 +11,4 @@ public class PointTests {
     Point p2 = new Point(2, 9);
     Assert.assertEquals(p1.distance(p2), 4.123105625617661);
   }
-
-  @Test
-  public void testDistanceNegative() {
-    Point p1 = new Point(6, 8);
-    Point p2 = new Point(2, 9);
-    Assert.assertEquals(p1.distance(p2), 4);
-  }
 }
